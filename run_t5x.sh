86c58030fff3735a8078839ba27a153ba5e2b367
Buzaaba <hbuzaaba@gmail.com>
2022-10-23 19:34:54 +0900

    added run_t5x.sh file

--- run_t5x.sh
+++ run_t5x.sh
@@ -1,3 +1,44 @@
+
+#$ -jc gpu-container_g1
+#$ -ac d=nvcr-tensorflow-2105-tf2-py3
+. /fefs/opt/dgx/env_set/nvcr-tensorflow-2105-tf2-py3.sh
+# Setting/unsetting proxy for internet access
+proxy_pcc() {
+    export MY_PROXY_URL=http://10.1.30.1:8080/
+    export HTTP_PROXY=$MY_PROXY_URL
+    export HTTPS_PROXY=$MY_PROXY_URL
+    export FTP_PROXY=$MY_PROXY_URL
+    export http_proxy=$MY_PROXY_URL
+    export https_proxy=$MY_PROXY_URL
+    export ftp_proxy=$MY_PROXY_URL
+}
+proxy_gpu() {
+    export MY_PROXY_URL="http://10.1.10.1:8080/"
+    export HTTP_PROXY=$MY_PROXY_URL
+    export HTTPS_PROXY=$MY_PROXY_URL
+    export FTP_PROXY=$MY_PROXY_URL
+    export http_proxy=$MY_PROXY_URL
+    export https_proxy=$MY_PROXY_URL
+    export ftp_proxy=$MY_PROXY_URL
+}
+proxy_clear() {
+    unset MY_PROXY_URL
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset ftp_proxy
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+}
+proxy_clear
+proxy_gpu
+
+### commented out: -pe OpenMP 28
+source $HOME/miniconda3/bin/activate base
+
+cd ${HOME}/Transformers/IvonNER/IVON_lang_tasks/t5x_Jax
+
+
 export GOOGLE_CLOUD_BUCKET_NAME=t5xtinetuning
 export TFDS_DATA_DIR=gs://$GOOGLE_CLOUD_BUCKET_NAME/t5x/data
 export MODEL_DIR=gs://$GOOGLE_CLOUD_BUCKET_NAME/t5x/$(date +%Y%m%d)
