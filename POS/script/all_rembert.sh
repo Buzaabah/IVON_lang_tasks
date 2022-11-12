c3e178410179ad84868a9a483d02a66bdde8da04
Buzaaba <hbuzaaba@gmail.com>
2022-11-12 13:31:41 +0900

    modified .sh

--- POS/script/all_rembert.sh
+++ POS/script/all_rembert.sh
@@ -1,3 +1,41 @@
+#$ -jc gpu-container_g1
+#$ -cwd
+#$ -ac d=nvcr-tensorflow-2105-tf2-py3
+. /fefs/opt/dgx/env_set/nvcr-tensorflow-2105-tf2-py3.sh
+# Setting/unsetting proxy for internet access
+proxy_pcc() {
+    export MY_PROXY_URL=http://10.1.30.1:8080/
+    export HTTP_PROXY=$MY_PROXY_URL
+    export HTTPS_PROXY=$MY_PROXY_URL
+    export FTP_PROXY=$MY_PROXY_URL
+    export http_proxy=$MY_PROXY_URL
+    export https_proxy=$MY_PROXY_URL
+    export ftp_proxy=$MY_PROXY_URL
+}
+proxy_gpu() {
+    export MY_PROXY_URL="http://10.1.10.1:8080/"
+    export HTTP_PROXY=$MY_PROXY_URL
+    export HTTPS_PROXY=$MY_PROXY_URL
+    export FTP_PROXY=$MY_PROXY_URL
+    export http_proxy=$MY_PROXY_URL
+    export https_proxy=$MY_PROXY_URL
+    export ftp_proxy=$MY_PROXY_URL
+}
+proxy_clear() {
+    unset MY_PROXY_URL
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset ftp_proxy
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+}
+proxy_clear
+proxy_gpu
+
+### commented out: -pe OpenMP 28
+source $HOME/miniconda3/bin/activate Torch_env
+
 for LANG in yor
 do
 	for j in 1 2 3 4 5 6 7
